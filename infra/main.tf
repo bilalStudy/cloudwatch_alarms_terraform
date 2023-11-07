4b2464e6848880e123521dcc89349a883e82d2e6
bilalStudy <biah001@student.kristiania.no>
2023-11-07 09:39:36 +0000

    all changes to complete task and workflow

--- infra/main.tf
+++ infra/main.tf
@@ -21,8 +21,33 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of accounts"
       }
+    },
+        {
+      "type": "metric",
+      "x": 0,
+      "y": 6,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.student_name}",
+            "bank_sum.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total bank sum"
+      }
     }
   ]
 }
 DASHBOARD
+}
+
+module "alarm" {
+  source = "./alarm_module"
+  alarm_email = var.alarm_email
+  prefix = var.student_name
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,8 @@
 variable "student_name" {
   type = string
+  default ="bilalahmed"
+}
+
+variable "alarm_email" {
+  type = string
 }
